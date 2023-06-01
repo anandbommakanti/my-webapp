d7f3596182524c0035f6d74671891713b969698e
Anand.cloud_user <anand@email>
2023-06-01 20:12:55 +0530

    Initial Commit

---
+++ src/main/java/com/anand/myapp/RestAPIs/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.anand.myapp.RestAPIs;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloWorld {
+    
+    @GetMapping(value = "/HelloWorld")
+    public String HelloWorld(){
+        return " Congratulations!, You have successfully launched the Application";
+    }
+}
---
+++ src/test/java/com/anand/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anand.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
